395abd2816cb1227a11feb61a07633e28f2ab3b7
Vatsal Trivedi <trivedivatsal005@gmail.com>
2016-09-17 19:45:43 +0530

    Delete Program7.asm

--- Program7.asm
+++
@@ -1,6 +0,0 @@
-;Find the 1's complement of the number stored at memory location 2200H and store the complemented number at memory location 2300H
-
-LDA 2200H	;Get the number
-CMA		;Complement number 
-STA 2300H	;Store the result
-HLT		;Terminate program execution
