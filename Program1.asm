398c693d10714b9c7e98b41f490bf3d6c8ae8e83
Vatsal Trivedi <trivedivatsal005@gmail.com>
2016-09-17 19:44:42 +0530

    Delete Program1.asm

--- Program1.asm
+++
@@ -1,6 +0,0 @@
-;Store the data byte 52h into memory location 2000H
-
-
-MVI A, 52H	;Store 52H in the accumulator
-STA 2000H	;Copy accumulator contents at address 2000h
-HLT		;Terminate program execution
