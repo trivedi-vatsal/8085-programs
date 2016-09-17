99a9e5579829a9d57e71b9f366cec412cee868cc
Vatsal Trivedi <trivedivatsal005@gmail.com>
2016-09-17 19:45:35 +0530

    Delete Program6.asm

--- Program6.asm
+++
@@ -1,9 +0,0 @@
-;Subtract the contents of memory locations 2001H from the memory location 2000H and place the result in memory location 2002H
-
-LXI H,2000H	;HL points 2000h
-MOV A,M		;Get first operand
-INX H		;HL points 2001H
-SUB M		;Subtract second operand
-INX H		;HL points 2002H
-MOV M,A		;Store result at 2002H
-HLT		;Terminate program execution
